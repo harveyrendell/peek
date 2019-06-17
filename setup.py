eebca90c7acf8c783142fe02394bc6c7aa8fb7cd
Harvey Rendell <harvey.rendell@pushpay.com>
2019-06-18 11:09:07 +1200

    Bump to 0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="peek",
-    version="0.1.0",
+    version="0.1.1",
     author="Harvey Rendell",
     author_email="hjrendell@gmail.com",
     description="Peek at python code",
