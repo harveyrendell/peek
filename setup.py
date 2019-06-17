a5981958a54cff26e47989e1a8aa2604a33d74b7
Harvey Rendell <harvey.rendell@pushpay.com>
2019-06-18 11:08:25 +1200

    Format with black

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='peek',
-    version='0.1.0',
-    author='Harvey Rendell',
-    author_email='hjrendell@gmail.com',
-    description='Peek at python code',
+    name="peek",
+    version="0.1.0",
+    author="Harvey Rendell",
+    author_email="hjrendell@gmail.com",
+    description="Peek at python code",
     packages=find_packages(),
 )
