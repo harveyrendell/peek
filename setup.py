b5818ba3d5363ef266c5022af37b1ca365273702
Harvey Rendell <harvey.rendell@pushpay.com>
2019-06-18 10:54:42 +1200

    Update module structure for better importing

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='Peek',
+    name='peek',
     version='0.1.0',
     author='Harvey Rendell',
     author_email='hjrendell@gmail.com',
